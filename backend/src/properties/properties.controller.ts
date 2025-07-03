67e7647407de593ef4a6331fbc45d0ff47f0c64e
Your Name <amanrpxx@gmail.com>
2025-07-03 13:38:17 +0530

    Wrote routes for all CRUD operations in property.services

--- backend/src/properties/properties.controller.ts
+++ backend/src/properties/properties.controller.ts
@@ -1,10 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { PropertiesService } from './properties.service';
+import { Properties } from './properties.entity';
 
 @Controller('properties')
 export class PropertiesController {
 
-    constructor(private readonly propertiesService: PropertiesService) {}
+    constructor(private readonly propertiesService: PropertiesService) { }
 
-    
+    @Get()
+    getAllProperties() {
+        return this.propertiesService.getAllProperties()
+    }
+
+    @Get(':id')
+    getPropertyById(@Param('id') id: number): Promise<Properties> {
+        return this.propertiesService.getPropertyById(id);
+    }
+
+    @Get('city/:city')
+    getPropertyByCity(@Param('city') city: string): Promise<Properties[]> {
+        return this.propertiesService.getPropertyByCity(city);
+    }
+
+    @Post()
+    createProperty(@Body() newProperty: { City: string, Address: string, ZipCode: string, Property_Type: string, Price: string, Square_Feet: number, Beds: number, Bathrooms: number, Features: string, Listing_Type: string },) {
+        return this.propertiesService.createProperty(newProperty)
+    }
+
+    @Put(':id')
+    updatePropertyById(@Param('id') id: number, @Body() updatedFields: { City: string, Address: string, ZipCode: string, Property_Type: string, Price: string, Square_Feet: number, Beds: number, Bathrooms: number, Features: string, Listing_Type: string }): Promise<Properties> {
+        return this.propertiesService.updatePropertyById(id, updatedFields);
+    }
+
+
+    @Delete(':id')
+    deletePropertyById(@Param('id') id: number) {
+        return this.propertiesService.deletePropertyById(+id);
+    }
 }
