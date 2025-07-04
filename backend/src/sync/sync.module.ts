26317340d680b265be89f478a7b90d454e0e6066
Your Name <amanrpxx@gmail.com>
2025-07-04 12:28:24 +0530

    Imported necessary modules

--- backend/src/sync/sync.module.ts
+++ backend/src/sync/sync.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SyncController } from './sync.controller';
 import { SyncService } from './sync.service';
+import { SheetsService } from 'src/sheets/sheets.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Properties } from '../properties/properties.entity';
 
 @Module({
+    imports:  [TypeOrmModule.forFeature([Properties])], 
     controllers: [SyncController],
-    providers: [SyncService]
+    providers: [SyncService, SheetsService]
 })
 export class SyncModule {}
