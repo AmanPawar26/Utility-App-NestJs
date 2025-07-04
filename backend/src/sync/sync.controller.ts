afffc44d1c223ea08a81d3a88898b3a0e794b480
Your Name <amanrpxx@gmail.com>
2025-07-04 12:26:58 +0530

    Routes for load and save

--- backend/src/sync/sync.controller.ts
+++ backend/src/sync/sync.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { SyncService } from './sync.service';
 
 @Controller('sync')
-export class SyncController {}
+export class SyncController {
+    constructor(private readonly syncService: SyncService) { }
+
+    @Get('load')
+    async loadData() {
+        return { message: await this.syncService.loadData() };
+    }
+
+    @Post('save')
+    async saveData() {
+        return { message: await this.syncService.saveData() };
+    }
+
+}
