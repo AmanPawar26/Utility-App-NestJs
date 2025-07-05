44e757a85bda7e2ebbfbc56d89ead6468af4ea27
Your Name <amanrpxx@gmail.com>
2025-07-05 13:03:03 +0530

    DTO validation for creating a property and updating a property

---
+++ backend/src/properties/dto/createProperty.dto.ts
@@ -0,0 +1,57 @@
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreatePropertyDto{
+    @IsString()
+    @IsNotEmpty()
+    City: string;
+
+    @IsString()
+    @IsNotEmpty()
+    Address: string;
+
+    @IsString()
+    @IsNotEmpty()
+    ZipCode: string;
+
+    @IsString()
+    @IsNotEmpty()
+    Property_Type: string;
+
+    @IsString()
+    @IsNotEmpty()
+    Price: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Type(() => Number)
+    Square_Feet: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Type(() => Number)
+    Beds: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Type(() => Number)
+    Bathrooms: number;
+
+    @IsString()
+    Features: string;
+
+    @IsString()
+    @IsNotEmpty()
+    Listing_Type: string;
+
+
+
+
+    
+
+
+
+    
+
+
+}
