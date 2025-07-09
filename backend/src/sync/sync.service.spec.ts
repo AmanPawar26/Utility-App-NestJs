c36d22350d06548e0c4d440b96f3a501c6ac6e1e
Your Name <amanrpxx@gmail.com>
2025-07-09 19:03:47 +0530

    Unit test for sync services to load data from sheet and save data to sheet

--- backend/src/sync/sync.service.spec.ts
+++ backend/src/sync/sync.service.spec.ts
@@ -1,12 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SyncService } from './sync.service';
+import { Repository } from 'typeorm';
+import { Properties } from 'src/properties/properties.entity';
+import { SheetsService } from 'src/sheets/sheets.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('SyncService', () => {
   let service: SyncService;
+  let propertyRepo: jest.Mocked<Partial<Repository<Properties>>>;
+  let sheetsService: jest.Mocked<SheetsService>;
+
+  const mockDataFromSheet = [
+    {
+      City: 'Test City',
+      Address: '123 Test Street',
+      ZipCode: '12345',
+      'Property Type': 'House',
+      Price: '500000',
+      'Square Feet': '2000',
+      Beds: '3',
+      Bathrooms: '2',
+      Features: 'Garage, Pool',
+      'Listing Type': 'Sale',
+    },
+  ];
+
+   const mockFormattedEntity = {
+    City: 'Test City',
+    Address: '123 Test Street',
+    ZipCode: '12345',
+    Property_Type: 'House',
+    Price: '500000',
+    Square_Feet: 2000,
+    Beds: 3,
+    Bathrooms: 2,
+    Features: 'Garage, Pool',
+    Listing_Type: 'Sale',
+  };
+
+
 
   beforeEach(async () => {
+    propertyRepo = {
+      clear: jest.fn(),
+      create: jest.fn().mockReturnValue(mockFormattedEntity),
+      save: jest.fn(),
+      find: jest.fn().mockResolvedValue([mockFormattedEntity])
+    };
+
+    sheetsService = {
+      loadDataFromSheet: jest.fn().mockResolvedValue(mockDataFromSheet),
+      saveDataToSheets: jest.fn(),
+    } as any;
+
+    
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SyncService],
+      providers: [
+        SyncService,
+        {provide: getRepositoryToken(Properties), useValue: propertyRepo},
+        {provide: SheetsService, useValue: sheetsService},
+      ],
     }).compile();
 
     service = module.get<SyncService>(SyncService);
@@ -15,4 +68,44 @@ describe('SyncService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('loadData', ()=>{
+    it('should load data from Google Sheets and save to DB', async () => {
+      const result = await service.loadData();
+
+      expect(sheetsService.loadDataFromSheet).toHaveBeenCalled();
+      expect(propertyRepo.clear).toHaveBeenCalled();
+      expect(propertyRepo.create).toHaveBeenCalledTimes(1);
+      expect(propertyRepo.save).toHaveBeenCalledWith([mockFormattedEntity]);
+      expect(result).toBe('Data loaded from Google Sheet into SQLite')
+    })
+  })
+  describe('saveData', ()=>{
+    it('should save data from DB to Google Sheets', async()=>{
+      const result = await service.saveData();
+
+      expect(propertyRepo.find).toHaveBeenCalled();
+      expect(sheetsService.saveDataToSheets).toHaveBeenCalledWith([
+        {
+          City: 'Test City',
+          Address: '123 Test Street',
+          ZipCode: '12345',
+          'Property Type': 'House',
+          Price: '500000',
+          'Square Feet': 2000,
+          Beds: 3,
+          Bathrooms: 2,
+          Features: 'Garage, Pool',
+          'Listing Type': 'Sale',
+        },
+      ]);
+      expect(result).toBe('Data saved to Google Sheet from SQLite.')
+    })
+
+    it('should throw error if saving data to sheet fails', async()=>{
+      sheetsService.saveDataToSheets.mockRejectedValue(new Error('Google Sheets error'));
+
+      await expect(service.saveData()).rejects.toThrow('Failed to save data to Google Sheets');
+    })
+  })
 });
