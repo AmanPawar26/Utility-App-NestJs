352de8e49ceffae54b5a12d93b272aec46397477
Your Name <amanrpxx@gmail.com>
2025-07-11 23:37:23 +0530

    Api service for frontend

---
+++ frontend/vite-project/src/components/Service/ApiService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3000';
+
+const ApiService = {
+    //CRUD
+    createProperty: (data) => axios.post(`${BASE_URL}/properties`, data),
+    getAllProperties: () => axios.get(`${BASE_URL}/properties`),
+    getPropertyById: (id) => axios.get(`${BASE_URL}/properties/${id}`),
+    getPropertiesByCity: (city) => axios.get(`${BASE_URL}/properties/city/${city}`),
+    updateProperty: (id, data) => axios.put(`${BASE_URL}/properties/${id}`, data),
+    deleteProperty: (id) => axios.delete(`${BASE_URL}/properties/${id}`),
+
+    //Sync
+    loadFromGoogleSheets: () => axios.get(`${BASE_URL}/sync/load`),
+    saveToGoogleSheets: () => axios.post(`${BASE_URL}/sync/save`),
+}
+
+export default ApiService
