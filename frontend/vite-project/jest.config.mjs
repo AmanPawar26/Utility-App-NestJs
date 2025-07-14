c79b2cbc1bee210f1770ea8c95d8a200e16e0ad9
Your Name <amanrpxx@gmail.com>
2025-07-14 15:16:45 +0530

    Jest and react testing library setup

---
+++ frontend/vite-project/jest.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+};
