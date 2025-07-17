acb53dbd34fd6ad22636b0f09c6dfadd8158ad17
Your Name <amanrpxx@gmail.com>
2025-07-17 11:40:55 +0530

    Included testmatch to ensure tests run as intended

--- frontend/vite-project/jest.config.mjs
+++ frontend/vite-project/jest.config.mjs
@@ -1,10 +1,14 @@
 export default {
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/__tests__/**/*.spec.[jt]s?(x)',
+  ],
 };
