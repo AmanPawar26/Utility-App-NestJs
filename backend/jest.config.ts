2613cfa8fbb5aa93a7dbe7635040bfa9438acc38
Aman Pawar <123646884+AmanPawar26@users.noreply.github.com>
2025-07-22 16:41:03 +0530

    Update jest.config.ts

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  rootDir: '.',
+  rootDir: '.', 
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
   },
   moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.(spec|int\\.spec)\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
