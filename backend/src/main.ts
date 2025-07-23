77238d88e8047fd2f5394c69b4622d100181efd1
Aman Pawar <123646884+AmanPawar26@users.noreply.github.com>
2025-07-23 15:54:52 +0530

    removed code that handles eact using nestjs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,31 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
-import { join } from 'path';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
     origin: 'http://localhost:5173',
-  });
-
-  const frontendPath = join(__dirname, '../frontend');
-  app.useStaticAssets(frontendPath);
-  app.use(express.static(frontendPath));
-
-
-  const expressApp = app.getHttpAdapter().getInstance();
-
-  expressApp.get('*', (req: express.Request, res: express.Response) => {
-    res.sendFile(join(frontendPath, 'index.html'));
   });
 
   const port = process.env.PORT || 7000;
