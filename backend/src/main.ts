207b1c03efa560909324f5e73a6b60690edb41ce
Aman Pawar <123646884+AmanPawar26@users.noreply.github.com>
2025-07-23 13:03:25 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
@@ -8,22 +9,22 @@ import { join } from 'path';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  // CORS for dev
   app.enableCors({
     origin: 'http://localhost:5173',
   });
 
-  // Serve frontend build
   const frontendPath = join(__dirname, '../frontend');
   app.useStaticAssets(frontendPath);
   app.use(express.static(frontendPath));
 
-  const httpAdapter = app.getHttpAdapter();
-  httpAdapter.get('*', (req, res) => {
+
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  expressApp.get('*', (req: express.Request, res: express.Response) => {
     res.sendFile(join(frontendPath, 'index.html'));
   });
 
@@ -33,3 +34,4 @@ async function bootstrap() {
 }
 
 bootstrap();
+
