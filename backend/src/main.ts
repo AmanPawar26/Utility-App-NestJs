e76cd9cac7aebf81286412cfa21dbc54db6027c0
Aman Pawar <123646884+AmanPawar26@users.noreply.github.com>
2025-07-24 17:55:35 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:5173', 'http://localhost'
+    origin: ['http://localhost:5173', 'http://localhost'],
   });
 
   const port = process.env.PORT || 7000;
@@ -20,4 +20,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
